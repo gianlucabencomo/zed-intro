13766ed349e61dee1bf3557cb41425c895a1d848
Gianluca Bencomo <gb5435@princeton.edu>
2025-09-15 14:39:34 -0700

    camera recording

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
 import argparse
 import pyzed.sl as sl
+import sys
+from signal import signal, SIGINT
 
 def main(args):
     zed = sl.Camera()
 
+    #Handler to deal with CTRL+C properly
+    def handler(signal_received, frame):
+        zed.disable_recording()
+        zed.close()
+        sys.exit(0)
+
+    signal(SIGINT, handler)
+
     init_params = sl.InitParameters()
+    init_params.camera_resolution = sl.RESOLUTION.HD1080
+    init_params.camera_fps = 30
     init_params.sdk_verbose = 0
 
     # Open camera
     e = zed.open(init_params)
     if e != sl.ERROR_CODE.SUCCESS:
         raise ValueError(f"ZED SDK raise the following error while attempting to open the camera: {e}")
-        
+
+    # Enable recording
+    recording_params = sl.RecordingParameters("./output.svo", sl.SVO_COMPRESSION_MODE.H264)
+    e = zed.enable_recording(recording_params)
+    if e != sl.ERROR_CODE.SUCCESS:
+        raise ValueError(f"ZED SDK raise the following error while attempting to enable recording: {e}")
+
+    
+    
+    # Serial Number
     zed_serial = zed.get_camera_information().serial_number
     print(f"ZED Mini Serial Number: {zed_serial}")
+
+    # Calibration Parameters
+    calibration_params = zed.get_camera_information().camera_configuration.calibration_parameters
+
+    i = 0
+    runtime_parameters = sl.RuntimeParameters()
+    print("SVO is Recording, use Ctrl-C to stop.")
+    while True:
+        if zed.grab(runtime_parameters) == sl.ERROR_CODE.SUCCESS:
+            print("Frame count: " + str(i), end="\r")
+            i += 1
 
     zed.close()
 
